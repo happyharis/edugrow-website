d743e3764039bcce5282efc7c5825df291e98229
happyharis <muhamad_haris@hotmail.com>
2025-01-06 16:02:09 +0800

    add analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,15 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astroship.web3templates.com",
   integrations: [tailwind(), mdx(), sitemap(), icon()],
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true, // set to false when using @vercel/analytics@1.4.0
+    },
+  }),
 });
